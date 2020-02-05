71c7f1ce4b722a5722822bb59af9586ea1505f0f
Nevin Wong Syum Ganesan <ngane103@uottawa.ca>
2020-02-05 09:40:18 -0500

    Update layout.js

--- web-app/frontend/src/components/layout.js
+++ web-app/frontend/src/components/layout.js
@@ -36,9 +36,12 @@ const Layout = ({ children }) => {
         <footer style={{ textAlign: "center" }}>
           Copyright © {new Date().getFullYear()}, Built Source
           {` `}
-          <a href="https://github.com/iDuckDark/CEG-Capstone">Code</a> {` `}
-          and {` `}
-          <a href="https://trello.com/b/1iLa3eKF/ceg-4912-capstone">Trello</a>
+          <a href="https://github.com/iDuckDark/CEG-Capstone">Code</a> {` `}, 
+          <a href="https://trello.com/b/1iLa3eKF/ceg-4912-capstone">
+            Trello
+          </a>{" "}
+          {` `} and
+          <a href="https://ceg-capstone.herokuapp.com/"> Server </a>
         </footer>
       </div>
     </>
