83728f64861fba9b702d4b002cb1e72567bbc895
Nevin Wong Syum Ganesan <ngane103@uottawa.ca>
2020-02-21 13:58:58 -0500

    Create gps.py

---
+++ drone/gps/gps.py
@@ -0,0 +1,13 @@
+# import requests
+# import json
+
+# send_url = 'http://freegeoip.net/json'
+# r = requests.get(send_url)
+# j = json.loads(r.text)
+# lat = j['latitude']
+# lon = j['longitude']
+
+
+import geocoder
+g = geocoder.ip('me')
+print(g.latlng)
