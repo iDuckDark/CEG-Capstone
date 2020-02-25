a4c38ef07f719b605f5a881307f1fb8428fa1db4
Nevin Wong Syum Ganesan <ngane103@uottawa.ca>
2020-02-25 17:04:24 -0500

    Radar

---
+++ web-app/frontend/src/pages/Radar/Radar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Layout from "../../components/layout";
+import Title from "../../components/Titles/Title";
+
+const Radar = () => {
+    return (
+        <div class='panel'>
+        </div>
+    );
+};
+export default Radar;
