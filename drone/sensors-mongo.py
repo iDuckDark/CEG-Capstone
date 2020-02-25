dfe00d36ac028716585482caadcc7b8961337d21
Raspberry-Pi <pfara039@uottawa.ca>
2020-02-24 22:06:54 -0500

    push temp alt and pressure to mongo

--- drone/sensors-mongo.py
+++ drone/sensors-mongo.py
@@ -1,6 +1,31 @@
 from pymongo import MongoClient
+from bmp280 import BMP280
+import time
 
+try:
+    from smbus2 import SMBus
+except ImportError:
+    from smbus import SMBus
+
+bus = SMBus(1)
+bmp280 = BMP280(i2c_dev=bus)
+
+# Connect to db
 client = MongoClient("mongodb+srv://root:root@cluster0-8kwlj.mongodb.net/test?retryWrites=true&w=majority")
+db = client.ssar
+# gps = db.gps
+sensors = db.sensors
+while(True):
+    temperature = bmp280.get_temperature()
+    pressure = bmp280.get_pressure()
+    altitude = bmp280.get_altitude()
+    sense_id = sensors.insert_one({"temperature":temperature,"pressure":pressure,"altitude":altitude}).inserted_id
+    time.sleep(10)
+
+
+
+
+
 # Example write to mongo, db = test, table = posts
 # db = client.test
 
