4f3999b62626a1e7273cd4ed3eb003af28ae1ede
Nevin Wong Syum Ganesan <ngane103@uottawa.ca>
2020-03-08 04:27:50 -0400

    fixed minor warnings

--- web-app/frontend/src/components/layout.js
+++ web-app/frontend/src/components/layout.js
@@ -8,7 +8,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
-import Header from "./header";
+// import Header from "./header";
 import "./layout.css";
 // import './index.scss';
 import { Header as HeaderTabs } from "../helpers/components";
