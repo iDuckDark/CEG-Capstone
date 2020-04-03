1feac2d85a3a9152d82e9806d8697fd764837c21
Nevin Wong Syum Ganesan <ngane103@uottawa.ca>
2020-04-03 17:58:00 -0400

    backend

--- web-app/backend/app.js
+++ web-app/backend/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
 const cors = require('cors');
 
 var indexRouter = require('./routes/index');
