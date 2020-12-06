5de2e73f8be6d43d88d00a27f2ef10f87d1fe828
Nevin Wong Syum Ganesan <ngane103@uottawa.ca>
2020-12-06 18:48:58 -0500

    clean up dashboard/graph

--- web-app/frontend/src/components/Graphs/Graph.jsx
+++ web-app/frontend/src/components/Graphs/Graph.jsx
@@ -8,76 +8,20 @@ import {
     ResponsiveContainer,
     YAxis,
 } from "recharts";
-import { shuffle, cleaner } from "../../helpers/utils";
 
 class Graph extends Component {
     constructor(props) {
         super(props);
-        const { name, color, refresh, width, height } = props;
-        let { data } = props;
+        const { name, color, width, height } = props;
+        const { data } = props;
         for (const item of data) item[name] = item.pv;
-        switch (name) {
-            case "Sound":
-                data = cleaner(name, 50, 75, 35);
-                break;
-            case "Altitude":
-                data = cleaner(name, 25, 50, 20);
-                break;
-            case "Battery":
-                data = cleaner(name, 80, 100, 15);
-                break;
-            case "CPUTemperature":
-                data = cleaner(name, 80, 95, 27);
-                break;
-            case "CPUUsage":
-                data = cleaner(name, 80, 98, 27);
-                break;
-            case "RAMUsage":
-                data = cleaner(name, 0.5, 1, 27);
-                break;
-            case "DiskUsage":
-                data = cleaner(name, 60, 100, 27);
-                break;
-            default:
-                break;
-        }
         this.state = {
             data,
             name,
             color,
-            refresh: refresh || false,
             width,
             height,
-            updateFunction: null,
         };
-        this.update = this.update.bind(this);
-    }
-
-    componentDidMount() {
-        const { refresh } = this.state;
-        if (refresh) {
-            const updateFunction = setInterval(() => this.update(), 300);
-            this.setState({ updateFunction });
-        }
-    }
-
-    componentWillUnmount() {
-        const { updateFunction } = this.state;
-        if (updateFunction) clearInterval(updateFunction);
-    }
-
-    update() {
-        const { data } = this.state;
-        const { name } = this.props;
-        let newData = shuffle(data);
-        if (name === "Battery") {
-            newData = newData.sort(this.sortFloat);
-        }
-        this.setState({ data: newData });
-    }
-
-    sortFloat(a, b) {
-        return b.Battery - a.Battery;
     }
 
     renderGraph() {
@@ -138,7 +82,6 @@ Graph.defaultProps = {
     data: null,
     name: null,
     color: null,
-    refresh: false,
     width: 260,
     height: 220,
 };
@@ -147,7 +90,6 @@ Graph.propTypes = {
     data: PropTypes.any,
     name: PropTypes.any,
     color: PropTypes.any,
-    refresh: PropTypes.bool,
     width: PropTypes.number,
     height: PropTypes.number,
 };
